efa62af50875c60402115ad3e5178a67d0f26401
kakeke <dedael2525@gmail.com>
2018-11-20 17:18:17 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '41567a04d0efef03de87679ee63b6ba0e4dc8fe758348af955711a5d3a90247d6d59eb97276f5a4823f4c53ddc26b859a20695d45cf4837566ea4fc0394b47ef'
